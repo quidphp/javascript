a719266bbed33b47be3ef2cdeedaf5c1b4183537
Pierre-Philippe Emond <emondpph@gmail.com>
2022-05-18 10:43:12 -0400

    dist name

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -1,8 +1,8 @@
 const mix = require('laravel-mix');
-mix.js('index.js', 'dist/javascript.js');
-mix.js('test/javascript.js', 'dist/javascript-test.js');
-mix.babel('dist/javascript.js', 'dist/javascript-es5.js');
-mix.babel('dist/javascript-test.js', 'dist/javascript-test-es5.js');
+mix.js('index.js', 'dist/javascript-es.js');
+mix.js('test/javascript.js', 'dist/javascript-test-es.js');
+mix.babel('dist/javascript-es.js', 'dist/javascript.js');
+mix.babel('dist/javascript-test-es.js', 'dist/javascript-test.js');
 mix.webpackConfig({
     output: {
         library: ["Quid"],
