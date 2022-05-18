ee568c58045f9afb8b604447570e613e2da8aad3
Pierre-Philippe Emond <emondpph@gmail.com>
2022-05-17 23:07:35 -0400

    fix header comment

--- src/validate.js
+++ src/validate.js
@@ -1,7 +1,7 @@
 /*
  * This file is part of the QuidPHP package <https://quidphp.com>
  * Author: Pierre-Philippe Emond <emondpph@gmail.com>
- * License: https://github.com/quidphp/node/blob/master/LICENSE
+ * License: https://github.com/quidphp/javascript/blob/master/LICENSE
  */
  
 // validate
--- test/run.js
+++ test/run.js
@@ -1,14 +1,14 @@
 /*
  * This file is part of the QuidPHP package <https://quidphp.com>
  * Author: Pierre-Philippe Emond <emondpph@gmail.com>
- * License: https://github.com/quidphp/node/blob/master/LICENSE
+ * License: https://github.com/quidphp/javascript/blob/master/LICENSE
  */
 
 // run
 // file to run the testsuite in node
 
 // import
-import { Test, TestSuite } from './node.js';
+import { Test, TestSuite } from './javascript.js';
 
 // testSuite
 TestSuite(Test);
