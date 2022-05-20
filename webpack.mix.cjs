ed0677b6ed19a649783b5a7399640246eae81f3c
Pierre-Philippe Emond <emondpph@gmail.com>
2022-05-19 23:11:42 -0400

    add laravel mix polyfill

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -1,8 +1,10 @@
 const mix = require('laravel-mix');
-mix.js('index.js', 'dist/javascript-es.js');
-mix.js('test/javascript.js', 'dist/javascript-test-es.js');
-mix.babel('dist/javascript-es.js', 'dist/javascript.js');
-mix.babel('dist/javascript-test-es.js', 'dist/javascript-test.js');
+require('laravel-mix-polyfill');
+
+mix.js('index.js', 'dist/javascript.js');
+mix.js('test/javascript.js', 'dist/javascript-test.js');
+mix.polyfill({ enabled: true, useBuiltIns: false });
+
 mix.webpackConfig({
     output: {
         library: ["Quid"],
